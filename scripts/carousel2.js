bc0e717acc6650a0955ba65d7eff283d378a8ef7
Seira <seiray117@gmail.com>
2019-05-07 11:32:28 -0700

    portfolio11

--- scripts/carousel2.js
+++ scripts/carousel2.js
@@ -3,11 +3,18 @@ var flkty = new Flickity( elem, {
   // options
   cellAlign: 'left',
   contain: true,
-  wrapAround:true
+  wrapAround:true,
 });
 
 // element argument can be a selector string
 //   for an individual element
 var flkty = new Flickity( '.main-carousel', {
   // options
+});
+
+var Flickity = require('flickity-fade');
+requirejs( [ 'path/to/flickity-fade' ], function( Flickity ) {
+  var flkty = new Flickity( '.carousel', {
+    fade: true,
+  });
 });
